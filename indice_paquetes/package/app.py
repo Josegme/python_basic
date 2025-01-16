fc942ea3724ec710985312497798eebcd22cd057
josegme <josegmescobar@gmail.com>
2025-01-16 18:32:08 -0300

    enviando algunos cambios - el modulo de paquetes se elimino

---
+++ indice_paquetes/package/app.py
@@ -0,0 +1,10 @@
+import requests 
+
+r = requests.get("https://www.google.com.co")
+print(r)
+#200 quiere decir ok
+#http status codes
+
+#ahora vamos a crear un ambiente virtual, por que instalamos el pip de manera local
+#python -m venv env
+#lib vamos a ver las dependencias instaladascl
